f83b97f404d60acaf50b2e0bb6e22d750cef3f37
Claudio Corridore <claudio.corridore@gmail.com>
2017-03-15 23:58:13 +0000

    lesson 6

--- lesson-6/app/app.js
+++ lesson-6/app/app.js
@@ -1,17 +1,69 @@
-var redis = require("redis"),
-    client = redis.createClient(6379, 'db');
+'use strict';
 
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const values        = require('object.values');
+const redis         = require('redis');
+const fs            = require('fs');
 
-client.set("string key", "string val", redis.print);
-client.hset("hash key", "hashtest 1", "some value", redis.print);
-client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-client.hkeys("hash key", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-    });
-    client.quit();
-});
+const PORT          = process.env.PORT          || '8080';
+const HOSTNAME      = process.env.HOSTNAME      || '127.0.0.1';
+const DB_PORT       = process.env.DB_PORT       || '6379';
+const DB_HOSTNAME   = process.env.DB_HOSTNAME   || 'localhost';
+
+const client = redis.createClient(DB_PORT, DB_HOSTNAME);
+const indexPageHtml = fs.readFileSync('./index.html', 'utf8');
+
+function startServer() {
+    const app = express();
+    const urlencodedParser = bodyParser.urlencoded({
+        extended: false
+    })
+
+    app
+        .use((req, res, next) => {
+            process.stdout.write(`${req.method}: ${req.url}\n`);
+            next();
+        })
+        .get('/', (req, res) => {
+            client.hgetall('form_data', (err, obj) => {
+                if (err) {
+                    return res.send(err.message);
+                }
+
+                const data = obj ? values(obj) : [];
+
+                res.send(indexPageHtml.replace('<ul>', '<ul>' + data.map(val => '<li>' + val + '</li>').join('')));
+            })
+        })
+        .post('/submit', urlencodedParser, (req, res) => {
+            const data = req.body.data;
+
+            if (data) {
+                const time = new Date().getTime().toString();
+
+                return client.hset('form_data', 'data_' + time, req.body.data, (err) => {
+                    if (err) {
+                        return res.send(err.message);
+                    }
+        
+                    res.redirect('/');
+                });
+            }
+
+            return res.send('Not empty string please');
+        })
+        .listen(PORT, HOSTNAME, () => {
+            process.stdout.write(`App is listening on http://${HOSTNAME}:${PORT}\n`);
+        })
+}
+
+client
+    .on('ready', startServer)
+    .on('error', (err) => {
+        process.stderr.write(`${err.message}\n`);
+
+        if (err.code === 'ECONNREFUSED') {
+            client.quit();
+        }
+    })
